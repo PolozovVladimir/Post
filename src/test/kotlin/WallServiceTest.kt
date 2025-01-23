6a3bce8bab01cf8f8e9d2731432275aaccdd5fd2
Vladimir Polozov <volodya06061991@gmail.com>
2025-01-23 18:47:53 +0300

    Commit

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -4,10 +4,11 @@ import Comments as Comments1
 
 class WallServiceTest{
 
-     private val service = WallService()
-     @Test
-     fun addExisting() {
 
+     @Test
+
+     fun addExisting() {
+         val service = WallService()
          val actual = service.add(Post(id = 1111111))
 
          assertTrue(actual.id != 0L)
@@ -15,7 +16,7 @@ class WallServiceTest{
 
      @Test
      fun updateExistingFalse() {
-
+         val service = WallService()
          val actual = service.update(Post(1111111))
 
          assertFalse(actual)
@@ -23,7 +24,7 @@ class WallServiceTest{
 
      @Test
      fun updateExistingTrue() {
-
+         val service = WallService()
          val addeds = service.add(Post(id = 1111111))
          val actual = service.update(addeds.copy(text = "Test"))
 
@@ -32,12 +33,14 @@ class WallServiceTest{
 
      @Test(expected = PostNotFoundException::class)
      fun shouldThrow() {
+         val service = WallService()
          val comment = Comment(1, 1, 1, "comment")
          service.createComment(3, comment)
 
      }
      @Test
      fun shouldWork(){
+         val service = WallService()
          val actual: Comment = service.createComment (1, comment = Comment(1,1,1,"comment"))
 
          assertTrue(actual.id != 1)
